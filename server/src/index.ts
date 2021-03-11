e4696dcdfbb30baf914618279fcd118bc5d03673
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-03-11 00:39:55 -0300

    #15 - fix server struture complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello Word!!")
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import {bibleAPI} from './api/bible.api';
+
+//// config
+const PORT = 1200;
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(cors({origin: true}));
+
+
+//// routes
+app.use('/bible',bibleAPI);
+
+app.listen(PORT, ()=> {
+    console.log('TBN - Server Application [started]');
+})
