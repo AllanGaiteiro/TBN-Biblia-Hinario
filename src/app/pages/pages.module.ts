08accd7a16b0ba37c976728c1ce219fa5d1df5e2
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2021-04-08 08:46:31 -0300

    modularizacao

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,20 @@
+
+// import { HinarioComponent } from './hinario/hinario.component';
+import { BibliaComponent } from './biblia/biblia.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+        HomeComponent,
+        BibliaComponent,
+        // HinarioComponent,
+    ],
+    exports: [
+
+    ],
+    imports: [
+    ]
+})
+
+export class PagesModule { }
